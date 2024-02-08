aa78d513afa9c3d85c90621941f1d27ea897d8f9
stepan323446 <stevedekart2020@gmail.com>
2024-02-08 19:49:31 +0100

    OOP

--- js/game.js
+++ js/game.js
@@ -1,216 +1,293 @@
-let scoreElement = document.getElementById("score");
-let gameCanvas = document.getElementById("game");
-let ctx = gameCanvas.getContext("2d");
+class Engine {
 
-let startModal = document.querySelector(".modal");
-let startBtn = document.getElementById("start-btn");
+    /**
+     * @param {HTMLCanvasElement} gameCanvas 
+     * @param {Number} fps 
+     */
+    constructor(gameCanvas, fps = 10) {
+        this.canvas = gameCanvas;
+        this.ctx = gameCanvas.getContext("2d");
 
-let score = 0;
+        this._playing = false
+        this._msPrev = window.performance.now(),
+            this.fps = fps
 
-const config = {
-    msPrev: window.performance.now(),
-    msPerFrame: 1000 / 10,
-
-    sizeCeil: 16,
-    playing: false,
-    radiusBerry: 6
-}
-const defaultSnake = {
-    x: 16,
-    y: 16,
-    dx: config.sizeCeil,
-    dy: 0,
-    tails: [],
-    maxTails: 20
-}
-let snake = { }
-let berry = {
-    x: 0,
-    y: 0
-}
-
-function Start() {
-    score = 0;
-    Object.assign(snake, defaultSnake);
-    snake.tails = [];
-    drawScore();
-
-    config.playing = true;
-    
-    randomPositionBerry();
-    FixedUpdate();
-    Update();
-
-    startModal.setAttribute("hidden", "");
-}
-function FixedUpdate() {
-    if(!config.playing)
-        return;
-
-    requestAnimationFrame(FixedUpdate)
-
-    // Stable frame rate on all screens
-    const msNow = window.performance.now()
-    const msPassed = msNow - config.msPrev
-
-    if (msPassed < config.msPerFrame) return
-
-    const excessTime = msPassed % config.msPerFrame;
-    config.msPrev = msNow - excessTime;
-
-    ctx.clearRect(0, 0, gameCanvas.offsetWidth, gameCanvas.offsetHeight)
-    
-    // Game Logic
-    drawBerry();
-    drawSnake();
-}
-function Update() {
-    requestAnimationFrame(Update);
-}
-
-function Stop() {
-    config.playing = false;
-    
-    startBtn.textContent = "Replay";
-    startModal.removeAttribute("hidden");
-}
-
-function drawSnake() {
-    snake.x += snake.dx;
-    snake.y += snake.dy;
-
-    // Collision with border
-    if(snake.x < 0)
-        Stop()
-    else if(snake.x >= gameCanvas.offsetWidth)
-        Stop()
-
-    if(snake.y < 0)
-        Stop()
-    else if(snake.y >= gameCanvas.offsetHeight)
-        Stop()
-
-    // Remove last tail item which extends beyond the maximum tail
-    snake.tails.unshift({ x: snake.x, y: snake.y });
-
-    if (snake.tails.length > snake.maxTails) {
-        snake.tails.pop();
+        this._UpdateEngine = this._UpdateEngine.bind(this)
+        this._FixedUpdateEngine = this._FixedUpdateEngine.bind(this)
+    }
+    get fps() {
+        return this._fps;
+    }
+    set fps(value) {
+        this._msPerFrame = 1000 / value,
+            this._fps = value
     }
 
-    // Create tail and head
-    snake.tails.forEach((el, i) => {
-        if(i == 0) 
-            ctx.fillStyle = "#FA0556"
-        else 
-            ctx.fillStyle = "#A00034" 
+    /**
+     * Start/Repeat Game
+     */
+    Start() {
+        // ....
 
-        ctx.fillRect(el.x, el.y, config.sizeCeil, config.sizeCeil)
-
-        // Collision with tail
-        if(i != 0) {
-            if(snake.tails[i].x == snake.x &&
-                snake.tails[i].y == snake.y)
-                Stop()
-        }
-    });
-    // Collision with food
-    if(snake.x == berry.x && snake.y == berry.y) {
-        incScore();
-        snake.maxTails++;
-        randomPositionBerry();
+        this._playing = true;
+        this._FixedUpdateEngine();
+        this._UpdateEngine();
     }
-
-    
-}
-const direction = {
-    right: "ArrowRight",
-    top: "ArrowUp",
-    left: "ArrowLeft",
-    bottom: "ArrowDown"
-}
-function control(direct) {
-    // We cannot turn in the opposite direction (if we go up, we cannot turn sharply down)
-    switch (direct) {
-        case direction.right:
-        case direction.left:
-            if(snake.dx != 0)
-                return;
-            break;
-
-        case direction.top:
-        case direction.bottom:
-            if(snake.dy != 0)
-                return;
-            break;
-
-        default:
+    /**
+     * Stop Game
+     */
+    Stop() {
+        this._playing = false
+    }
+    _UpdateEngine() {
+        if (!this._playing)
             return;
+
+        requestAnimationFrame(this.Update);
+
+        // .....
     }
+    _FixedUpdateEngine() {
+        if (!this._playing)
+            return;
+
+        requestAnimationFrame(this._FixedUpdateEngine)
+
+        // Stable frame rate on all screens
+        const msNow = window.performance.now()
+        const msPassed = msNow - this._msPrev
+
+        if (msPassed < this._msPerFrame) return
+
+        const excessTime = msPassed % this._msPerFrame;
+        this._msPrev = msNow - excessTime;
+
+        // Clear canvas for drawing
+        this.ctx.clearRect(0, 0, this.canvas.offsetWidth, this.canvas.offsetHeight);
+
+        this.FixedUpdate();
+    }
+
+    /**
+     * Fixed update frames by fps
+     */
+    FixedUpdate() { }
+    /**
+     * Update frames by monitor Hz
+     */
+    Update() { }
+}
+
+
+class Berry {
+    static color = "#A00034"
+
+    constructor(ctx) {
+        this.ctx = ctx;
+
+        this.RandomPosition()
+    }
+    /**
+     * Draw berry object on ctx
+     */
+    Draw() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = Berry.color;
+        this.ctx.arc(this.x + Snake2DGame.config.sizeCeil / 2, this.y + Snake2DGame.config.sizeCeil / 2, Snake2DGame.config.radiusBerry, 0, Math.PI * 2)
+        this.ctx.fill();
+    }
+    /**
+     * set random position using ctx and sizeCeil
+     */
+    RandomPosition() {
+        this.x = getRandomInt(0, gameCanvas.offsetWidth / Snake2DGame.config.sizeCeil) * Snake2DGame.config.sizeCeil
+        this.y = getRandomInt(0, gameCanvas.offsetHeight / Snake2DGame.config.sizeCeil) * Snake2DGame.config.sizeCeil
+    }
+}
+
+class Snake {
+    static direction = {
+        right: "ArrowRight",
+        top: "ArrowUp",
+        left: "ArrowLeft",
+        bottom: "ArrowDown"
+    }
+    static colors = {
+        headColor: "#FA0556",
+        tailColor: "#A00034"
+    }
+
+    constructor(ctx) {
+        this.ctx = ctx;
+
+        this.isDied = false;
+        // Coordinates
+        this.x = 16;
+        this.y = 16;
+
+        // Speed
+        this.dx = Snake2DGame.config.sizeCeil;
+        this.dy = 0;
+
+        // Tail
+        this.maxTails = Snake2DGame.config.maxTails;
+        this.tails = [];
+    }
+    /**
+     * Draw snake object on ctx
+     */
+    Draw() {
+        // Remove last tail item which extends beyond the maximum tail
+        this.tails.unshift({ x: this.x, y: this.y });
+
+        if (this.tails.length > this.maxTails) {
+            this.tails.pop();
+        }
+
+        // Create tail and head
+        this.tails.forEach((el, i) => {
+            // Zero index is head 
+            if (i == 0)
+                this.ctx.fillStyle = Snake.colors.headColor
+            else
+                this.ctx.fillStyle = Snake.colors.tailColor
+
+            this.ctx.fillRect(el.x, el.y, Snake2DGame.config.sizeCeil, Snake2DGame.config.sizeCeil)
+
+            // Collision with tail
+            if (i != 0) {
+                if (this.tails[i].x == this.x &&
+                    this.tails[i].y == this.y)
+                    this.Die();
+            }
+        });
+    }
+    /**
+     * Move using x, y
+     */
+    Move() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+    /**
+     * Change direction, using dx, dy. 
+     * @param {string} direct
+     */
+    Control(direct) {
+        // We cannot turn in the opposite direction (if we go up, we cannot turn sharply down)
+        switch (direct) {
+            case Snake.direction.right:
+            case Snake.direction.left:
+                if (this.dx != 0)
+                    return;
+                break;
     
-    snake.dx = 0;
-    snake.dy = 0;
+            case Snake.direction.top:
+            case Snake.direction.bottom:
+                if (this.dy != 0)
+                    return;
+                break;
+    
+            default:
+                return;
+        }
+    
+        this.dx = 0;
+        this.dy = 0;
+    
+        switch (direct) {
+            case Snake.direction.right:
+                this.dx = Snake2DGame.config.sizeCeil;
+                break;
+    
+            case Snake.direction.left:
+                this.dx = -Snake2DGame.config.sizeCeil;
+                break;
+    
+            case Snake.direction.top:
+                this.dy = -Snake2DGame.config.sizeCeil;
+                break;
+    
+            case Snake.direction.bottom:
+                this.dy = Snake2DGame.config.sizeCeil;
+                break;
+        }
+    }
+    /**
+     * Snake died
+     */
+    Die() {
+        this.isDied = true;
+    }
+}
+class Snake2DGame extends Engine {
+    static config = {
+        sizeCeil: 16,
+        radiusBerry: 6,
+        maxTails: 3,
+        incTail: 1
+    }
 
-    switch (direct) {
-        case direction.right:
-            snake.dx = config.sizeCeil;
-            break;
+    constructor({gameCanvas, onGameOver = () => {}, onStart = () => {}, onGameScore = (score) => {}}) {
+        super(gameCanvas, 10);
+        this.score = 0;
+        this.sizeCeil = 16
+        
+        this.onGameOver = onGameOver
+        this.onStart = onStart
+        this.onGameScore = onGameScore
+    }
+    Start() {
+        this.snake = new Snake(this.ctx);
+        this.berry = new Berry(this.ctx);
+        this.score = 0;
 
-        case direction.left:
-            snake.dx = -config.sizeCeil;
-            break;
+        super.Start();
+        
+        this.onStart();
+        this.onGameScore(this.score);
+    }
+    FixedUpdate() {
+        // Snake move
+        this.snake.Move();
 
-        case direction.top:
-            snake.dy = -config.sizeCeil;
-            break;
+        // CCollision with border
+        if (this.snake.x < 0)
+            this.snake.Die()
+        else if (this.snake.x >= this.canvas.offsetWidth)
+            this.snake.Die()
 
-        case direction.bottom:
-            snake.dy = config.sizeCeil;
-            break;
+        if (this.snake.y < 0)
+            this.snake.Die()
+        else if (this.snake.y >= this.canvas.offsetHeight)
+            this.snake.Die()
+
+        // Collision with berry
+        if (this.snake.x == this.berry.x && this.snake.y == this.berry.y) {
+            this.snake.maxTails += Snake2DGame.config.incTail;
+            this.berry.RandomPosition();
+            
+            this.score++;
+            this.onGameScore(this.score);
+        }
+
+        // Snake died = Game Over
+        if (this.snake.isDied) {
+            this.Stop();
+        }
+
+        // Draw objects
+        this.snake.Draw();
+        this.berry.Draw();        
+    }
+    Stop() {
+        super.Stop();
+        this.onGameOver();
     }
 }
 
-function drawBerry() {
-    ctx.beginPath();
-    ctx.fillStyle = "#A00034";
-    ctx.arc(berry.x + config.sizeCeil / 2, berry.y + config.sizeCeil / 2, config.radiusBerry, 0, Math.PI*2)
-    ctx.fill();
-}
-function randomPositionBerry() {
-    berry.x = getRandomInt(0, gameCanvas.offsetWidth / config.sizeCeil) * config.sizeCeil
-    berry.y = getRandomInt(0, gameCanvas.offsetHeight / config.sizeCeil) * config.sizeCeil
-}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * max);
 }
-  
-function incScore() {
-    score++;
-    drawScore();
-}
-function drawScore() {
-    scoreElement.innerText = score;
-}
 
-document.addEventListener("keydown", (e) => {
-    key = e.code;
-    switch (key) {
-        case "KeyW":
-            key = direction.top
-            break;
-        case "KeyS":
-            key = direction.bottom
-            break;
-        case "KeyA":
-            key = direction.left
-            break;
-        case "KeyD":
-            key = direction.right
-            break;
-    }
-    control(key);
-})
-startBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    Start();
-});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-// Start();
+let scoreElement = document.getElementById("score");
+let gameCanvas = document.getElementById("game");
+
+let startModal = document.querySelector(".modal");
+let startBtn = document.getElementById("start-btn");
+
+let gameSnake = new Snake2DGame({
+    gameCanvas: gameCanvas,
+    onGameScore: (score) => {
+        scoreElement.textContent = score;
+    },
+    onGameOver: () => {
+        startModal.removeAttribute("hidden");
+    }
+});
+
+startBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    startModal.setAttribute("hidden", "");
+    gameSnake.Start();
+});
+
+document.addEventListener("keydown", (e) => {
+    key = e.code;
+    switch (key) {
+        case "KeyW":
+            key = Snake.direction.top
+            break;
+        case "KeyS":
+            key = Snake.direction.bottom
+            break;
+        case "KeyA":
+            key = Snake.direction.left
+            break;
+        case "KeyD":
+            key = Snake.direction.right
+            break;
+    }
+    gameSnake.snake.Control(key);
+})
